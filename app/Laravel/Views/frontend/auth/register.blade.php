67d612252de266501ffb7826436743f0a8d287f5
josephjrdlc21 <josephdelacruzjr4@gmail.com>
2024-09-11 11:19:46 +0800

    fix register ui

--- app/Laravel/Views/frontend/auth/register.blade.php
+++ app/Laravel/Views/frontend/auth/register.blade.php
@@ -13,12 +13,8 @@
             <div class="card">
                 <div class="card-body">
                     <div class="m-sm-4">
-                        <div class="text-center mb-3">
-                            <img src="{{isset($settings) ? "{$settings->directory}/{$settings->filename}" : ""}}" alt="Logo" class="img-fluid" width="120">
-                        </div>
                         <form method="POST" action="">
                             {!!csrf_field()!!}
-                            @include('frontend._components.notification')
                             <div class="row">
                                 <div class="col-md-6">
                                     <div class="mb-3">
@@ -80,27 +76,25 @@
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="col-12">
+                                <div class="col-md-6">
                                     <div class="mb-3">
                                         <label class="form-label">Password</label>
                                         <input class="form-control form-control-lg" type="password" name="password" placeholder="Password" value="{{old('password')}}">
                                         @if($errors->first('password'))
                                         <small class="d-block mt-1 text-danger">{{$errors->first('password')}}</small>
                                         @endif
+                                        <small>
+                                            <a href="{{route('frontend.auth.login')}}">Already have an account? Sign in</a>
+                                        </small>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-12">
+                                <div class="col-md-6">
                                     <div class="mb-3">
                                         <label class="form-label">Confirm Password</label>
                                         <input class="form-control form-control-lg" type="password" name="password_confirmation" placeholder="Confirm Password" value="{{old('password_confirmation')}}">
                                         @if($errors->first('password_confirmation'))
                                         <small class="d-block mt-1 text-danger">{{$errors->first('password_confirmation')}}</small>
                                         @endif
-                                        <small>
-                                            <a href="{{route('frontend.auth.login')}}">Already have an account? Sign in</a>
-                                        </small>
                                     </div>
                                 </div>
                             </div>
