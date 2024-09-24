be1d07c254d5bde5e2236fe374edc6eae9d724f8
Joseph Dela Cruz Jr. <josephdelacruzjr4@gmail.com>
2024-09-24 11:24:08 +0800

    errors label adjustment

--- app/Laravel/Views/errors/500.blade.php
+++ app/Laravel/Views/errors/500.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')  
 <div class="text-center">
-    <div class="error mx-auto" data-text="505">505</div>
+    <div class="error mx-auto" data-text="500">500</div>
     <p class="lead text-gray-800 mb-5">Server Error</p>
     <p class="text-gray-500 mb-5">An unexpected error occurred. Please try again later...</p>
     <a href="{{route('portal.index')}}">← Back to Home</a>
